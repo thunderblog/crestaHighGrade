4df2bef1cb4507d3e124f4f7d3215f006a3b9062
thunderblog <hiroki3373@gmail.com>
2021-07-24 16:56:16 +0900

    windowスクロールでサイトロゴとハンバーガーメニューの色替え

---
+++ js/humburger.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+jQuery(function(){
+
+	$(window).scroll(function(){
+
+		const FIRST_VIEW_HEIGHT = $(".first-view__image").outerHeight() - 50;
+		const FIRST_VIEW_RIGHTBOX_HEIGHT = $('.first-view__right-box').outerHeight();
+
+		if($(window).scrollTop() > FIRST_VIEW_HEIGHT){
+			$("#main-title").css('background-image', 'url(../img/site-logo-black.png)');
+		}else{
+			$("#main-title").css('background-image', 'url(../img/site-logo.png)');
+		}
+
+		if($(window).scrollTop() > FIRST_VIEW_RIGHTBOX_HEIGHT){
+			$(".humburger-menu__button--line").css('background-color', 'black');
+		}else{
+			$(".humburger-menu__button--line").css('background-color', 'white');
+		}
+
+
+	});
+
+});
