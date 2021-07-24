e7c1eadace2c72fd2a8dddcad21e3567a8601e00
thunderblog <hiroki3373@gmail.com>
2021-07-24 23:17:24 +0900

    仮で完成！

--- js/humburger.js
+++ js/humburger.js
@@ -2,10 +2,11 @@
 
 jQuery(function(){
 
+	const FIRST_VIEW_RIGHTBOX_HEIGHT = $('.first-view__right-box').outerHeight();
+
 	$(window).scroll(function(){
 
 		const FIRST_VIEW_HEIGHT = $(".first-view__image").outerHeight() - 50;
-		const FIRST_VIEW_RIGHTBOX_HEIGHT = $('.first-view__right-box').outerHeight();
 
 		if($(window).scrollTop() > FIRST_VIEW_HEIGHT){
 			$("#main-title").css('background-image', 'url(../img/site-logo-black.png)');
@@ -24,12 +25,23 @@ jQuery(function(){
 		$(this).toggleClass('active');
 
 		if($(this).hasClass('active')){
-			$('.humburger-menu__nav').css('opacity', '100');
+			$('.humburger-menu__nav').css('opacity','100');
+			$('.humburger-menu__overlay').css('height', '100vh');
 			$('.body').addClass('no-scroll');
 			$(".humburger-menu__button--line").css('background-color', 'white');
+			$("#main-title").css('background-image', 'url(../img/site-logo.png)');
 		}else{
 			$('.humburger-menu__nav').css('opacity', '0');
+			$('.humburger-menu__overlay').css('height', '0');
 			$('.body').removeClass('no-scroll');
+
+			if($(window).scrollTop() > FIRST_VIEW_RIGHTBOX_HEIGHT){
+				$(".humburger-menu__button--line").css('background-color', 'black');
+				$("#main-title").css('background-image', 'url(../img/site-logo-black.png)');
+			}else{
+				$(".humburger-menu__button--line").css('background-color', 'white');
+				$("#main-title").css('background-image', 'url(../img/site-logo.png)');
+			}
 		}
 	});
 });
